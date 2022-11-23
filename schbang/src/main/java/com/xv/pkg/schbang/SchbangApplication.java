2bd3b753ed5c2486f054fce7ab4e4f7194d0c730
Lishant Sahu <lishantsahu@gmail.com>
2022-11-24 01:08:20 +0530

    app create

---
+++ schbang/src/main/java/com/xv/pkg/schbang/SchbangApplication.java
@@ -0,0 +1,13 @@
+package com.xv.pkg.schbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchbangApplication.class, args);
+	}
+
+}
